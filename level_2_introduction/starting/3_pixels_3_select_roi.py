1f9143c5ae91260e27af9cc1a38d0a695359544d
Haziel Covarrubias <haziel.covarrubias@outlook.com>
2025-03-22 13:22:48 -0700

    Actualizar 3_pixels_3_select_roi.py para seleccionar y mostrar regiones de interés en una imagen

--- level_2_introduction/starting/3_pixels_3_select_roi.py
+++ level_2_introduction/starting/3_pixels_3_select_roi.py
@@ -1,24 +1,50 @@
 import cv2
-import numpy as np
 
 # Se obtiene la ruta del archivo actual
-llave_1 = cv2.imread(
-    "c:/CodeSpace/Python/OpenCV/level_2_introduction/starting/pixels_llave.jpeg"
-)  # Se lee la imagen a color
-llave_2 = cv2.imread(
-    "c:/CodeSpace/Python/OpenCV/level_2_introduction/starting/pixels_llave_oxidada.jpeg"
-)  # Se lee la imagen a color
-alto, largo, canales = llave_1.shape  # Se obtienen las dimensiones de la imagen
+llaves = cv2.imread("pixels_llaves.jpg")  # Se lee la imagen a color
+llaves_copy = llaves.copy()  # Se crea una copia de la imagen
+alto, largo, canales = llaves.shape  # Se obtienen las dimensiones de la imagen
 
-# Se recorre la imagen
-candado1 = llave_1[0:alto, 0 : int(largo / 2)]
-candado2 = cv2.resize(llave_2[0:alto, int(largo / 2) :], (candado1.shape[1], alto))
+""" Seleccionar la región de interés de la imagen "(llave_1)" """
+cv2.namedWindow("llaves", cv2.WINDOW_NORMAL)  # Se crea una ventana
+roi_llaves = cv2.selectROI("llaves", llaves)
+print(roi_llaves)
 
-# Unir las dos imágenes
-imagen_unida = np.zeros((int(alto), int(largo), int(canales)), np.uint8)
-imagen_unida[0:alto, 0 : int(largo / 2)] = candado1
-imagen_unida[0:alto, int(largo / 2) : int(largo / 2) + candado2.shape[1]] = candado2
+# Se obtiene la región de interés de la imagen
+llave1 = llaves[
+    roi_llaves[1] : roi_llaves[1] + roi_llaves[3],
+    roi_llaves[0] : roi_llaves[0] + roi_llaves[2],
+]
+# Se obtienen las dimensiones de la imagen
+alto_llave1, largo_llave1, canales_llave1 = llave1.shape
 
-cv2.namedWindow("candado", cv2.WINDOW_NORMAL)
-cv2.imshow("candado", imagen_unida)  # Se muestra la imagen
-cv2.waitKey()  # Se espera a que se presione una tecla
+""" Seleccionar la región de interés de la imagen "(llave_2)" """
+roi_llave_2 = cv2.selectROI("llave_2", llaves)
+print(roi_llave_2)
+
+# Se obtiene la región de interés de la imagen
+llave2 = llaves[
+    roi_llave_2[1] : roi_llave_2[1] + roi_llave_2[3],
+    roi_llave_2[0] : roi_llave_2[0] + roi_llave_2[2],
+]
+# Se obtienen las dimensiones de la imagen
+alto_llave2, largo_llave2, canales_llave2 = llave2.shape
+
+""" Mostrar la imagen con la región de interés seleccionada """
+nueva_llave_1 = cv2.resize(llave1, (largo_llave2, alto_llave2))
+nueva_llave_2 = cv2.resize(llave2, (largo_llave1, alto_llave1))
+
+# Se copian las nuevas llaves en la imagen original
+llaves_copy[
+    roi_llaves[1] : roi_llaves[1] + roi_llaves[3],
+    roi_llaves[0] : roi_llaves[0] + roi_llaves[2],
+] = nueva_llave_1
+
+llaves_copy[
+    roi_llave_2[1] : roi_llave_2[1] + roi_llave_2[3],
+    roi_llave_2[0] : roi_llave_2[0] + roi_llave_2[2],
+] = nueva_llave_2
+
+cv2.imshow("llaves", llaves_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
